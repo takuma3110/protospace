bdc63d3e62c79535526bb4ac087b57f29540a1fd
Takuma Saito <nitol5524@gmail.com>
2016-04-28 01:40:02 +0900

    Add user's routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
+  resources :users, only: [:index, :edit, :show]
 
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
