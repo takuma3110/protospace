424cb36db9ba83baec9de33b7a0c9bc6f56e3221
Takuma Saito <nitol5524@gmail.com>
2016-05-12 02:47:32 +0900

    Edit prototypes files to create prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,8 +4,22 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
+
+  def create
+    @prototype = current_user.prototypes.new(create_params)
+    @prototype.save
+    redirect_to action: :index
+    end
 
   def show
   end
+
+  private
+  def create_params
+        params.require(:prototype).permit(:title, :catch_copy, :concept, user_id: current_user.id)
+
+  end
 end
+
