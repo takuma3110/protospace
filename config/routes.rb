06aaaae35dfe83b5c4cd422229fff0591c13eb9f
Takuma Saito <nitol5524@gmail.com>
2016-05-13 23:25:24 +0900

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: [:edit, :show, :update]
-  resources :prototypes
+  resources :prototypes, only: [:index, :show, :new, :create]
 end
 
