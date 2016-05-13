e8b2f1b237a55306d881a99b8cbd8020b6611b58
Takuma Saito <nitol5524@gmail.com>
2016-05-14 00:26:07 +0900

    Add option dependent: :destroy

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :thumbnails
+  has_many :thumbnails, dependent: :destroy
   accepts_nested_attributes_for :thumbnails
   validates :title, :catch_copy, :concept, presence: true
 end
