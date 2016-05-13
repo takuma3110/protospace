6e88009bd865e21f6a281e99ad1f0ae94c449677
Takuma Saito <nitol5524@gmail.com>
2016-05-14 00:19:23 +0900

    Add validation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,6 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails
   accepts_nested_attributes_for :thumbnails
-  validates :title, presence: true
+  validates :title, :catch_copy, :concept, presence: true
 end
 
--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -2,5 +2,7 @@ class Thumbnail < ActiveRecord::Base
   belongs_to :prototype
   mount_uploader :image, ImageUploader
   enum status: [ :main, :sub ]
+  validates :image, presence: true
+
 end
 
