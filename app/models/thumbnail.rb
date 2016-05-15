a9d0cc44a28b15cb798ad42c95e3543832bcb5f4
Takuma Saito <nitol5524@gmail.com>
2016-05-16 01:40:12 +0900

    Add validation

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -2,7 +2,7 @@ class Thumbnail < ActiveRecord::Base
   belongs_to :prototype
   mount_uploader :image, ImageUploader
   enum status: [ :main, :sub ]
-  validates :image, presence: true
+  validates :image, :status presence: true
 
 end
 
