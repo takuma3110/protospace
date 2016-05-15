6ef699d1c3e11cb0c11377f02b167fff99ca1d7e
Takuma Saito <nitol5524@gmail.com>
2016-05-15 19:25:52 +0900

    Add flash message and error message

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,17 +9,21 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = current_user.prototypes.new(create_params)
-    @prototype.save
-    redirect_to action: :index
+    @prototype = current_user.prototypes.new(prototype_params)
+    if @prototype.save
+      redirect_to action: :index
+    else
+      flash[:alert] = "failed"
+      render new_prototype_path
     end
+  end
 
   def show
   end
 
   private
-  def create_params
-        params.require(:prototype).permit(:title, :catch_copy, :concept, user_id: current_user.id ,thumbnails_attributes: [:image, :status, :prototype_id])
+  def prototype_params
+        params.require(:prototype).permit(:title, :catch_copy, :concept, :user_id ,thumbnails_attributes: [:image, :status, :prototype_id])
 
   end
 end
