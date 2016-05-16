fd8ac56caf41ba5fecc16e45a2757e3b3db94c8d
Takuma Saito <nitol5524@gmail.com>
2016-05-16 20:39:27 +0900

    Change uploads to use default image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,7 +16,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-     "/images/fallback/" + [version_name, "noimag.png"].compact.join('_')
+     "/images/fallback/" + [version_name, "default.png"].compact.join('_')
    end
 
   # Process files as they are uploaded:
