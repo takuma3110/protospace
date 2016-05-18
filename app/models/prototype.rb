24cb36672a40bf675e34d6b216ffa1a52ff5ce37
Takuma Saito <nitol5524@gmail.com>
2016-05-18 21:32:31 +0900

    Add association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails, dependent: :destroy
+  has_one :main_image, class_name: "Thumbnail", foreign_key: "prototype_id"
   accepts_nested_attributes_for :thumbnails
   validates :title, :catch_copy, :concept, presence: true
 end
