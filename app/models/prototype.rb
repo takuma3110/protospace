238b167370ef1697d65e5dc2634f19cbf58dba98
Takuma Saito <nitol5524@gmail.com>
2016-05-25 00:39:33 +0900

    Edit association to define without magic number

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,8 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails, dependent: :destroy
-  has_one :main_image, -> { where(status: 0) }, class_name: "Thumbnail"
-  has_many :sub_images, -> { where(status: 1)}, class_name: "Thumbnail"
+  has_one :main_image, -> {where(status: Thumbnail.statuses[:main])}, class_name: "Thumbnail"
+  has_many :sub_images, -> {where(status: Thumbnail.statuses[:sub])}, class_name: "Thumbnail"
   accepts_nested_attributes_for :thumbnails, reject_if: :reject_posts
   validates :title, :catch_copy, :concept, presence: true
 
