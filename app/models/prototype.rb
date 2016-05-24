f5a7767313b51e97667a5af1ef8344250a91f2ea
Takuma Saito <nitol5524@gmail.com>
2016-05-24 18:58:54 +0900

    Add option of accepts_nested_attributes

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,7 +3,11 @@ class Prototype < ActiveRecord::Base
   has_many :thumbnails, dependent: :destroy
   has_one :main_image, -> { where(status: 0) }, class_name: "Thumbnail"
   has_many :sub_images, -> { where(status: 1)}, class_name: "Thumbnail"
-  accepts_nested_attributes_for :thumbnails
+  accepts_nested_attributes_for :thumbnails, reject_if: :reject_posts
   validates :title, :catch_copy, :concept, presence: true
+
+  def reject_posts(attributed)
+    attributed['image'].blank?
+  end
 end
 
