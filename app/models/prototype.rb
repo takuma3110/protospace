5d25b18b48c016b97dbcaf231f668a88dc5e2b12
Takuma Saito <nitol5524@gmail.com>
2016-05-28 23:47:08 +0900

    Add association at prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,9 +3,13 @@ class Prototype < ActiveRecord::Base
   has_many :thumbnails , dependent: :destroy
   has_one :main_image, -> {where(status: Thumbnail.statuses[:main])}, class_name: "Thumbnail"
   has_many :sub_images, -> {where(status: Thumbnail.statuses[:sub])}, class_name: "Thumbnail"
-  has_many :likes
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :thumbnails, reject_if: :reject_posts
   validates :title, :catch_copy, :concept, presence: true
+
+  def like_user(user)
+   likes.find_by(user_id: user.id)
+  end
 
   def reject_posts(attributed)
     attributed['image'].blank?
