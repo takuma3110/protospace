03020c17b80b9f7860bdff0225ba938b568fa3f9
Takuma Saito <nitol5524@gmail.com>
2016-05-28 18:02:39 +0900

    Create likes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512103214) do
+ActiveRecord::Schema.define(version: 20160528085308) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
