bedbfb388268ece372b926eae584c86c9d6e47ef
Takuma Saito <nitol5524@gmail.com>
2016-05-30 00:26:22 +0900

    Delete comment out

--- app/controllers/prototypes/likes_controller.rb
+++ app/controllers/prototypes/likes_controller.rb
@@ -3,15 +3,10 @@ class Prototypes::LikesController < ApplicationController
   before_action :set_prototype
 
   def create
-    # @like = @prototype.likes.create(user_id: current_user.id)
-    # @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype.likes.create(params.permit(:user_id))
   end
 
   def destroy
-    # @like = current_user.likes.find_by(prototype_id: params[:prototype_id])
-    # @like.destroy
-    # @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype.likes.find(params[:id]).destroy
   end
 
