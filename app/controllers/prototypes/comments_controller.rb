834a431a6a355427aeeb56af4cbe08a0832e321d
Takuma Saito <nitol5524@gmail.com>
2016-06-01 17:22:12 +0900

    Add destroy action at comments controller

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,14 +1,25 @@
 class Prototypes::CommentsController < ApplicationController
 
-before_action :set_prototype
+before_action :set_prototype, only: :create
+before_action :set_comment, only: :destroy
 
   def create
     @prototype.comments.create(comment_params)
   end
 
+  def destroy
+    @prototype = Prototype.find(@comment.prototype_id)
+    @comment.destroy
+    redirect_to @prototype, success: "deleting is successful"
+  end
+
   private
   def set_prototype
     @prototype  = Prototype.find(params[:comment][:prototype_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
   def comment_params
