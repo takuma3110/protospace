5a248991f8932051ff6c545011aa1d7c62c62ab9
Takuma Saito <nitol5524@gmail.com>
2016-06-01 17:24:27 +0900

    Add routing of destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   scope module: :prototypes do
     resources :likes, only: [:create, :destroy]
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
 
   end
 end
