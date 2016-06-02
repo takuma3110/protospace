af32c99188d9538e40fecfeb9858384c39c52f5e
Takuma Saito <nitol5524@gmail.com>
2016-06-02 23:56:27 +0900

    Edit routing for delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: [:edit, :show, :update]
-  resources :prototypes
 
-  scope module: :prototypes do
-    resources :likes, only: [:create, :destroy]
-    resources :comments, only: [:create, :destroy]
-
+  resources :prototypes do
+    scope module: :prototypes do
+      resources :likes, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
   end
 end
 
