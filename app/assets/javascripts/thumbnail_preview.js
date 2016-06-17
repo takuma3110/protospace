5465f634600c90df985e085432983d0ff2682433
Takuma Saito <nitol5524@gmail.com>
2016-06-18 02:51:00 +0900

    Create js file to show thumbnail's preview

---
+++ app/assets/javascripts/thumbnail_preview.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('.thumbnail_preview').each(function() {
+    var self = $(this);
+    var input = self.find('input[type=file]');
+    input.change(function() {
+      var file = $(this).prop('files')[0];
+      var fileReader = new FileReader();
+      if (!this.files.length) {
+        return
+      }
+      if (!file.type.match('image.*')) {
+        return
+      }
+      fileReader.onload = function() {
+        self.css({
+          'background-image': 'url(' + fileReader.result + ')'
+        });
+      };
+      fileReader.readAsDataURL(file);
+    })
+  })
+})
