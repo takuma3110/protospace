a78cdcd41b94e828667d4c97c9683944f5239c36
Takuma Saito <nitol5524@gmail.com>
2016-06-21 03:45:41 +0900

    Delete a part of define self

--- app/assets/javascripts/thumbnail_preview.js
+++ app/assets/javascripts/thumbnail_preview.js
@@ -2,9 +2,8 @@ $(function() {
 
   var ids = [$('#js_thumbnail_preview'), $('#js_thumbnail_preview_0'), $('#js_thumbnail_preview_1'), $('#js_thumbnail_preview_2'), $('#js_thumbnail_preview_3')];
 
-  $.each(ids, function() {
-    var self = $(this)
-    let input = $(this).find('input[type=file]');
+  $.each(ids, function(i, value) {
+    const input = $(this).find('input[type=file]');
     input.change(function() {
       let file = $(this).prop('files')[0];
       let fileReader = new FileReader();
@@ -15,7 +14,7 @@ $(function() {
         window.alert('error!')
       }
       fileReader.onload = function() {
-        self.css({
+        value.css({
           'background-image': 'url(' + fileReader.result + ')'
         });
       };
