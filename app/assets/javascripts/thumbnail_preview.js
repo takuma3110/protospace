c8f2da174507e3def964c6ff64edcada3a47d187
Takuma Saito <nitol5524@gmail.com>
2016-06-21 04:51:38 +0900

    Change definition of 'this'

--- app/assets/javascripts/thumbnail_preview.js
+++ app/assets/javascripts/thumbnail_preview.js
@@ -3,10 +3,10 @@ $(function() {
   const ids = [$('#js_thumbnail_preview'), $('#js_thumbnail_preview_0'), $('#js_thumbnail_preview_1'), $('#js_thumbnail_preview_2'), $('#js_thumbnail_preview_3')];
 
   $.each(ids, function(i, value) {
-    const input = $(this).find('input[type=file]');
+    const input = value.find('input[type=file]');
     input.change(function() {
-      let file = $(this).prop('files')[0];
-      let fileReader = new FileReader();
+      var file = $(this).prop('files')[0];
+      const fileReader = new FileReader();
       if (!this.files.length) {
         window.alert('error!')
       }
