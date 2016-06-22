ffe2a62a7feb9b82383c7f2d0c04d552f1f0dcf2
Takuma Saito <nitol5524@gmail.com>
2016-06-23 02:03:19 +0900

    Edit carrierwave.rb to use storage file when test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
     config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    aws_access_key_id: Settings.S3[:access_key_id],
+    aws_secret_access_key: Settings.S3[:secret_access_key],
+    region: Settings.S3[:region]
   }
 
     case Rails.env
@@ -14,5 +14,8 @@ CarrierWave.configure do |config|
     when 'production'
         config.fog_directory  = Settings.S3[:directory]
         config.asset_host = Settings.S3[:asset_host]
+    end
+    when 'test'
+        config.storage = :file
     end
 end
