ff2dee642d5b07449f2f461d2ce932566e04f350
Takuma Saito <nitol5524@gmail.com>
2016-06-23 23:54:11 +0900

    Edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,20 @@
 CarrierWave.configure do |config|
-    config.storage = :fog
+  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.S3[:access_key_id],
-    aws_secret_access_key: Settings.S3[:secret_access_key],
-    region: Settings.S3[:region]
+    aws_access_key_id: S3.access_key,
+    aws_secret_access_key: S3.secret_access_key,
+    region: S3.region
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = Settings.S3[:directory]
-        config.asset_host = Settings.S3[:asset_host]
+        config.fog_directory  = S3.directory
+        config.asset_host = S3.asset_host
+    end
     when 'production'
-        config.fog_directory  = Settings.S3[:directory]
-        config.asset_host = Settings.S3[:asset_host]
+        config.fog_directory  = S3.directory
+        config.asset_host = S3.asset_host
     end
     when 'test'
         config.storage = :file
