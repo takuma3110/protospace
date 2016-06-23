a5e8b43961998f8cd89475627bd2feebcdedef7b
Takuma Saito <nitol5524@gmail.com>
2016-06-24 00:04:24 +0900

    Change S3 to s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,19 +2,19 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: S3.access_key,
-    aws_secret_access_key: S3.secret_access_key,
-    region: S3.region
+    aws_access_key_id: s3.access_key,
+    aws_secret_access_key: s3.secret_access_key,
+    region: s3.region
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = S3.directory
-        config.asset_host = S3.asset_host
+        config.fog_directory  = s3.directory
+        config.asset_host = s3.asset_host
     end
     when 'production'
-        config.fog_directory  = S3.directory
-        config.asset_host = S3.asset_host
+        config.fog_directory  = s3.directory
+        config.asset_host = s3.asset_host
     end
     when 'test'
         config.storage = :file
