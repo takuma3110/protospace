1dece0655bfbc829678ad002c8aa6623e1a6205b
Takuma Saito <nitol5524@gmail.com>
2016-07-03 02:28:51 +0900

    Add authenticate_user! to redirect sign_in page when user has not sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :success, :info, :warning, :danger
