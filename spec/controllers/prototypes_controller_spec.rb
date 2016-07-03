02175231dacf9255f65a3f7fd89fa1bf737b1926
Takuma Saito <nitol5524@gmail.com>
2016-07-03 21:48:49 +0900

    Do test index action

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -2,13 +2,26 @@
 require 'rails_helper'
 
 describe PrototypesController do
-   let(:prototype) { create(:prototype, :with_sub_images) }
+   let!(:prototype) { create(:prototype, :with_sub_images) }
    let(:params) {{
      id: prototype.id,
-     prototype: attributes_for(:prototype, title: 'sample', tag_list: 'sample')
+     prototype: attributes_for(:prototype, title: 'sample')
    }}
    let(:invalid_params) {{
      id: prototype.id,
-     prototype: attributes_for(:prototype, title: nil, tag_list: 'sample')
+     prototype: attributes_for(:prototype, title: nil)
    }}
+
+  describe 'with user login' do
+    before { login_user }
+    describe 'GET #index' do
+      before do
+        get :index
+      end
+
+      it 'assigns the requested prototypes to @prototypes' do
+        expect(assigns(:prototypes)).to include prototype
+      end
+    end
+  end
 end
