92c76005e60b243574f90bbb5bc54a82e7b4ed87
Takuma Saito <nitol5524@gmail.com>
2016-07-04 22:40:26 +0900

    Do test of destroy action about redirecting sign_in page

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -227,5 +227,12 @@ describe PrototypesController do
         expect(response).to redirect_to new_user_session_path
       end
     end
+
+    describe 'DELETE #destroy' do
+      it 'redirects sign_in page' do
+        delete :destroy, id: prototype
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
