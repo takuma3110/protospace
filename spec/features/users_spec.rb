24bacfab56bf8964f51436e6d7370ec8ba0debbe
Takuma Saito <nitol5524@gmail.com>
2016-07-07 06:09:02 +0900

    Do test of create a new prototype

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature 'user', type: :feature do
 
   given(:user) { build(:user) }
   given(:login_user) { create(:user) }
+  given(:prototype) { build(:prototype, :with_sub_images) }
+
 
   it 'creates new user', js: true do
     visit root_path
@@ -26,5 +28,23 @@ RSpec.feature 'user', type: :feature do
     fill_in 'user_password', with: login_user.password
     click_button 'Sign in'
     expect(page).to have_selector '.alert-notice', text: 'Signed in successfully.'
+  end
+
+  it 'creates a new prototype', js: true do
+    visit root_path
+    click_on 'Get Started'
+    fill_in 'user_email', with: login_user.email
+    fill_in 'user_password', with: login_user.password
+    click_button 'Sign in'
+    click_on 'New Proto'
+    fill_in 'prototype_title', with: prototype.title
+    fill_in 'prototype_catch_copy', with: prototype.catch_copy
+    fill_in 'prototype_concept', with: prototype.concept
+    attach_file 'prototype[thumbnails_attributes][0][image]', File.join(Rails.root, '/spec/fixtures/img/sample.png'),  visible: false
+    3.times do |i|
+      attach_file "prototype[thumbnails_attributes][#{i}][image]", File.join(Rails.root, '/spec/fixtures/img/sample.png'), visible: false
+    end
+    click_on '投稿する'
+    expect(page).to have_selector '.alert-success', text: 'success'
   end
 end
